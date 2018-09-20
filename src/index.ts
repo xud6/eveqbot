df71ad54ed450d865306014f668afaaed68b0c6b
徐岱青 <email@xdq.me>
2018-09-20 23:34:54 +0800

    add cq-websocket

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,27 @@
-function hello(compiler: string) {
-    console.log(`Hello from ${compiler}`);
+import CQWebSocket from 'cq-websocket'
+import { WebsocketType } from 'cq-websocket'
+
+let cqwebConfig = {
+  access_token: "",
+  host: "172.81.230.235",
+  port: 6700
 }
-hello("TypeScript");
+
+console.log(cqwebConfig)
+
+let bot = new CQWebSocket(cqwebConfig)
+
+bot.on('socket.connecting', function (wsType, attempts) {
+  console.log(`attemp to connect ${wsType} No.${attempts} started`)
+}).on('socket.connect', function (wsType, sock, attempts) {
+  console.log(`attemp to connect ${wsType} No.${attempts} success`)
+}).on('socket.failed', function (wsType, attempts) {
+  console.log(`attemp to connect ${wsType} No.${attempts} failed`)
+})
+
+bot.on('message', (event, context) => {
+  console.log(event);
+  console.log(context)
+})
+
+bot.connect()
