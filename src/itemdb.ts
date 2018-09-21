4e461903b0e675c5e58e07abad6165fcb832debd
徐岱青 <email@xdq.me>
2018-09-21 18:53:37 +0800

    Rename column name to EN to ease usage

--- src/itemdb.ts
+++ src/itemdb.ts
@@ -3,5 +3,20 @@ var workbook = XLSX.readFile('itemdb.xls');
 
 let itemSheet = workbook.Sheets[workbook.SheetNames[0]]
 
-export const itemData = XLSX.utils.sheet_to_json(itemSheet);
+// Rename column name to EN to ease usage
+itemSheet.B1.v = 'name';
+itemSheet.B1.w = 'name';
+itemSheet.C1.v = 'description';
+itemSheet.C1.w = 'description';
+itemSheet.D1.v = 'group1';
+itemSheet.D1.w = 'group1';
+itemSheet.E1.v = 'group2';
+itemSheet.E1.w = 'group2';
+itemSheet.F1.v = 'group3';
+itemSheet.F1.w = 'group3';
+itemSheet.G1.v = 'group4';
+itemSheet.G1.w = 'group4';
+itemSheet.H1.v = 'group5';
+itemSheet.H1.w = 'group5';
 
+export const itemData = XLSX.utils.sheet_to_json(itemSheet);
