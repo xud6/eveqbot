9bd50c85d19d749dac974e88bcf3cc6d6721c8a2
徐岱青 <email@xdq.me>
2018-09-22 00:43:33 +0800

    only show blueprint when input has blueprint

--- src/itemdb/index.ts
+++ src/itemdb/index.ts
@@ -3,12 +3,28 @@ import { filter, map, words, join } from 'lodash';
 
 export class cItemdb {
     readonly itemData: itemDataType[]
+    readonly itemDataBlueprint: itemDataType[]
+    readonly itemDataNoneBluepront: itemDataType[]
     constructor(readonly dataXlsName: string) {
         this.itemData = loadFromCeveMarketXLS(dataXlsName);
+        this.itemDataBlueprint = filter(this.itemData, item => {
+            if (item.name.indexOf("蓝图") >= 0) {
+                return true
+            } else {
+                return false
+            }
+        })
+        this.itemDataNoneBluepront = filter(this.itemData, item => {
+            if (item.name.indexOf("蓝图") >= 0) {
+                return false
+            } else {
+                return true
+            }
+        })
     }
-    searchByFullName(name: string) {
+    searchByFullName(name: string, itemData: itemDataType[]) {
         console.time('Fullname search complete in ')
-        let res = filter(this.itemData, item => {
+        let res = filter(itemData, item => {
             if (item.name.indexOf(name) >= 0) {
                 return true
             } else {
@@ -18,9 +34,9 @@ export class cItemdb {
         console.timeEnd('Fullname search complete in ')
         return res
     }
-    searchByWord(name: string) {
+    searchByWord(name: string, itemData: itemDataType[]) {
         console.time('Word search complete in ')
-        let result = this.itemData
+        let result = itemData
         let eWords = words(name, /(\d+)|(\w+)|[^(?:,& )]/g);
         console.log('word explode result :' + join(eWords, '|'))
         map(eWords, word => {
@@ -35,11 +51,20 @@ export class cItemdb {
         console.timeEnd('Word search complete in ')
         return result;
     }
+    switchBlueprintData(name: string): itemDataType[] {
+        if (name.indexOf('蓝图') >= 0) {
+            //has blueprint in name
+            return this.itemDataBlueprint;
+        }else{
+            return this.itemDataNoneBluepront;
+        }
+    }
     search(name: string) {
         console.time('search complete in ')
-        let res = this.searchByFullName(name);
+        let itemData = this.switchBlueprintData(name)
+        let res = this.searchByFullName(name, itemData);
         if (res.length == 0) {
-            res = this.searchByWord(name);
+            res = this.searchByWord(name, itemData);
         }
         console.timeEnd('search complete in ')
         return res
