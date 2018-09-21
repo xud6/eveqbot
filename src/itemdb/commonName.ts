f8dfe69ed1f58ff34b95f5585b1e6d9de2fd163f
徐岱青 <email@xdq.me>
2018-09-22 00:30:02 +0800

    init commonName handle

---
+++ src/itemdb/commonName.ts
@@ -0,0 +1,13 @@
+interface commonNameTableObj {
+    o:string,
+    t:string
+}
+
+const commonNameTable:commonNameTableObj[]=[
+    {o:"微曲",t:"微型跃迁推进器"},
+    {o:"反跳",t:"跃迁扰断器"},
+    {o:"网子",t:"停滞缠绕光束"},
+    {o:"T2",t:"II"},
+    {o:"t2",t:"II"},
+    {o:"ii",t:"II"}
+]
