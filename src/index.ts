54105a4ff4ef1f956f67a95f79044acf6a65d0ab
徐岱青 <email@xdq.me>
2018-09-22 18:28:07 +0800

    fix env

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,16 @@ import { cQQBot } from './bot';
 import { CQWebSocketOption } from 'cq-websocket';
 
 let cqwebConfig:Partial<CQWebSocketOption> = {}
-if(process.env.host){
+if(process.env.coolq_host){
     console.log('read config from env')
-    cqwebConfig.host = process.env.host;
-    if(process.env.port){
-        cqwebConfig.port = parseInt(process.env.port)
+    cqwebConfig.host = process.env.coolq_host;
+    if(process.env.coolq_port){
+        cqwebConfig.port = parseInt(process.env.coolq_port)
     }else{
         cqwebConfig.port = 6700
     }
-    if(process.env.access_token){
-        cqwebConfig.access_token = process.env.access_token
+    if(process.env.coolq_access_token){
+        cqwebConfig.access_token = process.env.coolq_access_token
     }else{
         cqwebConfig.access_token = ''
     }
