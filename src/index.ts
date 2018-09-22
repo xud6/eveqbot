e1c927100340c7949d19ac7afcb8690dfd3adc53
徐岱青 <email@xdq.me>
2018-09-22 17:45:11 +0800

    support read config from env

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,24 @@ import { cCEVEMarketApi } from './CEveMarketApi';
 import { cQQBot } from './bot';
 import { CQWebSocketOption } from 'cq-websocket';
 
-let cqwebConfig:Partial<CQWebSocketOption> = require('./../config.json')
+let cqwebConfig:Partial<CQWebSocketOption> = {}
+if(process.env.host){
+    console.log('read config from env')
+    cqwebConfig.host = process.env.host;
+    if(process.env.port){
+        cqwebConfig.port = parseInt(process.env.port)
+    }else{
+        cqwebConfig.port = 6700
+    }
+    if(process.env.access_token){
+        cqwebConfig.access_token = process.env.access_token
+    }else{
+        cqwebConfig.access_token = ''
+    }
+}else{
+    console.log('read config from config.json')
+    cqwebConfig = require('./../config.json')
+}
 
 console.log(cqwebConfig)
 let itemdb = new cItemdb('itemdb.xls');
