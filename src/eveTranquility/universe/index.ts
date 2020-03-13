a08f8e1efd9858da0902de41bffffb4a9ab2dd97
徐岱青 <xdq@hzky.xyz>
2020-03-13 13:00:50 +0800

    remove old module

--- src/eveTranquility/universe/index.ts
+++ src/eveTranquility/universe/index.ts
@@ -1,22 +1,17 @@
 import { tLogger } from "tag-tree-logger"
-import { eveTypesInfo } from "./typesInfo";
 import { eveTranquilityExtService } from "../types";
 
 
 export class eveUniverse {
     readonly logger: tLogger
-    typesInfo: eveTypesInfo
     constructor(
         readonly parentLogger: tLogger,
         readonly extService: eveTranquilityExtService
     ) {
         this.logger = parentLogger.logger(["universe"])
-        this.typesInfo = new eveTypesInfo(this.logger, this.extService)
     }
     async startup() {
-        await this.typesInfo.startup()
     }
     async shutdown() {
-        await this.typesInfo.shutdown()
     }
 }
