2b6133c5b725ee6fa765c2f7bcc1bda191331840
徐岱青 <xdq@hzky.xyz>
2020-03-16 09:45:41 +0800

    add persistent in opId

--- src/opId/index.ts
+++ src/opId/index.ts
@@ -1,9 +1,49 @@
+
+export interface tOpIdKvs {
+    get: (key: string) => Promise<string | null>
+    set: (key: string, value: string | null) => Promise<void>
+}
+
 export class opId {
     currentId: number;
+    kvs: tOpIdKvs | null = null
+    kvsKey: string = "opid"
+    kvsSaveTimer: NodeJS.Timeout | null
+    kvsSaveInterval_s: number = 10
     constructor() {
         this.currentId = 0;
     }
+    async startup() {
+        this.kvsSaveTimer = setInterval(() => {
+            if (this.kvs) {
+                this.kvs.set(this.kvsKey, this.currentId.toString())
+            }
+        }, this.kvsSaveInterval_s * 1000)
+    }
+    async shutdown() {
+        if (this.kvsSaveTimer) {
+            clearInterval(this.kvsSaveTimer)
+        }
+    }
     getId() {
         return this.currentId++
+    }
+    async setPersistentKvs(kvs: tOpIdKvs, key: string = "srvOpIdCurrent") {
+        this.kvs = kvs;
+        this.kvsKey = key;
+        let dbid: number = 0;
+        try {
+            let v = await this.kvs.get(this.kvsKey)
+            if (v) {
+                let i = parseInt(v);
+                if (i) {
+                    dbid = i
+                }
+            }
+        } catch (e) {
+
+        }
+        this.currentId = this.currentId + dbid + this.kvsSaveInterval_s * 100
+        await this.kvs.set(this.kvsKey, this.currentId.toString())
     }
 }
