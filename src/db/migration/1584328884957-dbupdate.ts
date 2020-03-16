12bb873536aabacec0d15f351fec5c7f00ef9ba8
徐岱青 <xdq@hzky.xyz>
2020-03-16 17:18:39 +0800

    fix NULL db migration

--- src/db/migration/1584328884957-dbupdate.ts
+++ src/db/migration/1584328884957-dbupdate.ts
@@ -4,11 +4,11 @@ export class dbupdate1584328884957 implements MigrationInterface {
     name = 'dbupdate1584328884957'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE `eve_esi_universe_systems` CHANGE `stargates` `stargates_raw` text NULL DEFAULT 'NULL'", undefined);
+        await queryRunner.query("ALTER TABLE `eve_esi_universe_systems` CHANGE `stargates` `stargates_raw` text NULL DEFAULT NULL", undefined);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE `eve_esi_universe_systems` CHANGE `stargates_raw` `stargates` text NULL DEFAULT 'NULL'", undefined);
+        await queryRunner.query("ALTER TABLE `eve_esi_universe_systems` CHANGE `stargates_raw` `stargates` text NULL DEFAULT NULL", undefined);
     }
 
 }
--- src/db/migration/1584328962890-dbupdate.ts
+++ src/db/migration/1584328962890-dbupdate.ts
@@ -4,11 +4,11 @@ export class dbupdate1584328962890 implements MigrationInterface {
     name = 'dbupdate1584328962890'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE `eve_esi_universe_systems` CHANGE `stations` `stations_raw` text NULL DEFAULT 'NULL'", undefined);
+        await queryRunner.query("ALTER TABLE `eve_esi_universe_systems` CHANGE `stations` `stations_raw` text NULL DEFAULT NULL", undefined);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE `eve_esi_universe_systems` CHANGE `stations_raw` `stations` text NULL DEFAULT 'NULL'", undefined);
+        await queryRunner.query("ALTER TABLE `eve_esi_universe_systems` CHANGE `stations_raw` `stations` text NULL DEFAULT NULL", undefined);
     }
 
 }
